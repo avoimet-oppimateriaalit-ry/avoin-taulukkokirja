0fa067ad2abab39d84281b14942c52c1d1d96d81
Joonas Mäkinen <joonasd6@gmail.com>
2015-09-25 23:47:05 +0300

    hakemistojen siistimistä

--- KOOSTA.sh
+++ KOOSTA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f kooste.html
 while IFS='' read -r line || [[ -n $line ]]; do
-    sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> kooste.html
+    sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> master.html
 done < "$1"
 
--- biologia/KOOSTA.sh
+++ biologia/KOOSTA.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -f kooste.html
 while IFS='' read -r line || [[ -n $line ]]; do
-    sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> kooste.html
+    sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> ../bilsakooste.html
 done < "$1"
-
