2a221275debf99660c0a29d064384f23f016b9b5
Joonas Mäkinen <joonasd6@gmail.com>
2015-09-26 18:28:40 +0300

    alettu kasata funktioita

--- kemia/KOOSTA.sh
+++ kemia/KOOSTA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f kooste.html
+rm -f ../kemia.html
 while IFS='' read -r line || [[ -n $line ]]; do
     sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> ../kemia.html
 done < "$1"
--- matematiikka/KOOSTA.sh
+++ matematiikka/KOOSTA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f kooste.html
+rm -f ../matematiikka.html
 while IFS='' read -r line || [[ -n $line ]]; do
     sed -n '/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA ALKAA -->/,/<!---PÄÄDOKUMENTTIIN KOPIOITAVA OSA LOPPUU -->/p' $line >> ../matematiikka.html
 done < "$1"
